222d34e4ed8594aece3caf00a71c5702d7418a92
Aaron <aaronmeade9292@gmail.com>
2018-07-07 16:21:43 -0700

    fixed errors

--- src/api.js
+++ src/api.js
@@ -5,11 +5,11 @@ import Prey from '../models/prey.js';
 
 const router = express.Router();
 
-let sendJSON = (res,data) => {
+let sendJSON = (res, data) => {
   res.statusCode = 200;
   res.statusMessage = 'OK';
   res.setHeader('Content-Type', 'application/json');
-  res.write( JSON.stringify(data) );
+  res.write(JSON.stringify(data));
   res.end();
 };
 
@@ -30,26 +30,22 @@ router.post('/api/v1/predator', (req, res) => {
 
 router.put('/api/v1/predator/:id', (req, res) => {
 
-  // Animals.find({_id:req.params.id}).then(animal => animal.update({...}))
-  Predators.findByIdAndUpdate(
-  req.params.id,
-  req.body,
-  {new: true},
-).then(data => {
+  Predators.findByIdAndUpdate(req.params.id, req.body, {new: true}).then(data => {
     sendJSON(res, data);
   }).catch(err => res.send(err));
-  
 });
-router.post('api/v1/prey', (req ,res, next) => {
+router.post('api/v1/prey', (req, res, next) => {
 
   Prey.create(req.body)
-    .then(results => {res.send(results);})
+    .then(results => {
+      res.send(results);
+    })
     .catch(next);
 });
 
 router.get('api/v1/prey', (req, res, next) => {
   Prey.find()
-    .then(results => {
+    .then(() => {
       Prey.find()
         .populate('Predators')
         .exec()
